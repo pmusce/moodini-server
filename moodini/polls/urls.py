8524d63c279c0d7639b50dc7a60b06c57c3067a7
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-03-15 13:56:25 +0100

    Mood url

--- moodini/polls/urls.py
+++ moodini/polls/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^create', views.start_poll, name='start_poll'),
     url(r'^(?P<poll_id>[0-9]+)/location/(?P<location_id>[0-9]+)/$', views.get_location, name='get_location'),
     url(r'^(?P<poll_id>[0-9]+)/location/(?P<location_id>[0-9]+)/vote/$', views.vote_location, name='vote_location'),
-    url(r'^(?P<poll_id>[0-9]+)/result/$', views.get_result, name='get_result')
+    url(r'^(?P<poll_id>[0-9]+)/result/$', views.get_result, name='get_result'),
+    url(r'^(?P<poll_id>[0-9]+)/mood/$', views.select_mood, name='select_mood')
 ]
